c02a6dfceffcb5673e7a1668097a559affab6501
doonnor <doconnor@numerix.com>
2022-05-08 08:00:06 +0000

    add projects

--- pages/projects.js
+++ pages/projects.js
@@ -16,8 +16,13 @@ export default function Projects() {
             <Title title="Projects" />
             
             <main className={styles.main}>  
-                <Card title="Fume Hood" link="/projects/project01" description="The fume hood I built for my home lab"/>
-                <Card title="Solvent Extraction" link="/projects/project02" description="Simple distilation of acetone from nail varnish remover"/>
+                <div className={styles.grid}>
+                    <Card title="Fume Hood" link="/projects/project01" description="The fume hood I built for my home lab"/>
+                    <Card title="Solvent Extraction" link="/projects/project02" description="Simple distilation of acetone from nail varnish remover"/>
+                    <Card title="Synthesizing RFNA" link="/projects/project03" description="Creating concentrated nitric acid from a nitrate salt"/>
+                    <Card title="Runaway Nitration" link="/projects/project04" description="Nitrating a solvent gone wrong"/>
+                    <Card title="Laser Canon" link="/projects/project05" description="Shoulder mounted death ray of death"/>
+                </div>
             </main>
         </div>
     )
